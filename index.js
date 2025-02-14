06ce1e8121debf9a6896c53b9d9b32b6ae723d12
Bea Serrano Lucio <78020428+BeaSerrano@users.noreply.github.com>
2025-02-14 18:30:38 +0100

    port index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 const { dbConnection } = require('./config/config');
 const routes = require('./routes');
 const swaggerUI = require('swagger-ui-express')
